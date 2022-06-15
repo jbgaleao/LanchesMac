74f7e18644ab9cfc848222dce2006365ccc13628
JOAO BATISTA GALEAO <jbgaleao@gmail.com>
2022-06-15 04:10:16 -0300

    auça 10 - Criação do projeto ASP .NET Core MVC

--- LanchesMac/Controllers/HomeController.cs
+++ LanchesMac/Controllers/HomeController.cs
@@ -8,13 +8,7 @@ namespace LanchesMac.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+        
         public IActionResult Index()
         {
             return View();
