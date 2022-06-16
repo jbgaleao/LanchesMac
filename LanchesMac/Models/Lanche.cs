1aa1ade1dbd7ca5b0e567faaf595cfc662980cbb
JOAO BATISTA GALEAO <jbgaleao@gmail.com>
2022-06-16 18:14:47 -0300

    Criando o Modelo de Dominio

---
+++ LanchesMac/Models/Lanche.cs
@@ -0,0 +1,18 @@
+﻿namespace LanchesMac.Models
+{
+    public class Lanche
+    {
+        public int LancheId { get; set; }
+        public string Name { get; set; }
+        public string DescricaoCurta { get; set; }
+        public string DescricaoDetalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string imagemUrl{ get; set; }
+        public string ImagemThumbnail { get; set; }
+        public bool IsLanchPreferido { get; set; }
+        public bool EmEstoque { get; set; }
+
+        public int CategoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; }
+    }
+}
