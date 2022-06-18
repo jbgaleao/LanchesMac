22263a9b978018f126b69bb708e14b121165a5e9
Joao Galeao <jbgaleao@gmail.com>
2022-06-18 19:50:52 -0300

    Conceitos : ViewData, ViewBag e TempData

--- LanchesMac/Controllers/LancheController.cs
+++ LanchesMac/Controllers/LancheController.cs
@@ -7,14 +7,22 @@ namespace LanchesMac.Controllers
     {
         private readonly ILancheRepository _lancheRepository;
 
-        public LancheController(ILancheRepository lancheRepository)
-        {
-            _lancheRepository = lancheRepository;
-        }
+        public LancheController(ILancheRepository lancheRepository) => _lancheRepository = lancheRepository;
 
         public IActionResult List()
-        {
+        {            
             var lanches = _lancheRepository.Lanches;
+
+            ViewData["titulo"] = "TODOS OS LANCHES";
+            ViewData["datahora"] = DateTime.Now;
+
+            TempData["nome"] = "JBGaleao";
+
+            ViewBag.texto = "Total de lanches listados: ";
+            ViewBag.total = lanches.Count();
+
+
+
             return View(lanches);
         }
     }
